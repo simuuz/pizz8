ea68a68daf91cf1a1daec9d6cacd2cb87c49b569
simuuz <simococo03@gmail.com>
2020-12-16 12:43:02 +0100

    initial commit

---
+++ build/CMakeFiles/chip-8.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/simuuz/Documents/chip-8/src/core/cpu.cpp" "/home/simuuz/Documents/chip-8/build/CMakeFiles/chip-8.dir/src/core/cpu.cpp.o"
+  "/home/simuuz/Documents/chip-8/src/core/mem.cpp" "/home/simuuz/Documents/chip-8/build/CMakeFiles/chip-8.dir/src/core/mem.cpp.o"
+  "/home/simuuz/Documents/chip-8/src/main.cpp" "/home/simuuz/Documents/chip-8/build/CMakeFiles/chip-8.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../include/core"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/chip-8.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chip-8.dir/src/core/cpu.cpp.o"
+  "CMakeFiles/chip-8.dir/src/core/mem.cpp.o"
+  "CMakeFiles/chip-8.dir/src/main.cpp.o"
+  "chip-8"
+  "chip-8.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chip-8.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
